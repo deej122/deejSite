be61506dc40bae860d2e137c4ffa9682d16c5af7
DJ Jagannathan <desjag@gmail.com>
2013-12-20 13:22:11 -0500

    redesign

---
+++ scripts/theme.js
@@ -0,0 +1,29 @@
+          $("document").ready(function(){
+          $("img#rbTheme").click(function() {
+            $('img#byTheme').removeClass("black_yellow");
+            })            
+          $('img#rbTheme').click(function(){
+            $('img#defTheme').removeClass("default");
+          })
+          $("img#rbTheme").click(function() {
+            $('img#rbTheme').addClass("red_blue");
+            })
+          $('img#rbTheme').click(function(){
+            $('div#name').addClass("blueBG");
+          })          
+          $("img#rbTheme").click(function() {
+            $('div#name').addClass("blackText");
+            })
+          $("img#rbTheme").click(function() {
+            $('div#bio').addClass("whiteBG");
+            })            
+          $('img#rbTheme').click(function(){
+            $('div#bio').addClass("blackText");
+          })
+          $("img#rbTheme").click(function() {
+            $('div#stuffContent').addClass("redBG");
+            })
+          $('img#rbTheme').click(function(){
+            $('div#stuffContent').addClass("blackText");
+          });
+          });          
